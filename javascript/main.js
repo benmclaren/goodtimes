ffce50d928f89da965d71ab95aa73c88e8ebef20
Ben McLaren <benmclaren@live.co.uk>
2020-09-21 20:24:53 +0100

    smooth scrolling

--- javascript/main.js
+++ javascript/main.js
@@ -184,3 +184,24 @@ const stripeHandle = function(paymentMethod) {
       )
     })
 }
+
+// smooth scrolling
+// grab all the anchor tags on the page
+const anchors = document.querySelectorAll('a')
+// loop over them
+anchors.forEach(anchor => {
+  // listen for clicks on each one
+  anchor.addEventListener('click', event => {
+    // grab the href attribute
+    const href = anchor.getAttribute('href')
+    // if the href starts with a #
+    if (href.charAt(0) === '#') {
+      // stop the default action
+      event.preventDefault()
+      // find the element the href points to and scroll it into view
+      document.querySelector(href).scrollIntoView({
+        behavior: 'smooth'
+      })
+    }
+  })
+})
